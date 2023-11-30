3be80ac1caa5ec99dbe2f1000f03d78f5b4245da
John Tocher <jmtocher@gmail.com>
2023-12-01 08:16:30 +1100

    Create puzzle_01-part_1_jmt.py
    Puzzle 1 code template

---
+++ Puzzle_01/puzzle_01-part_1_jmt.py
@@ -0,0 +1,11 @@
+""" Advent of code 2023 - Puzzle 01
+
+    https://adventofcode.com/2023/day/1
+
+    John Tocher     
+    Solution to puzzle 01 part 1
+"""
+
+INPUT_FILE_NAME = "puzzle_01_input.txt"
+
+print(f"Nothing to see here, move along!")
